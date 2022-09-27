e7dbb2b9aad45046bcd88e389b291321431b9296
rnretina <92212590+oveeernight@users.noreply.github.com>
2022-09-28 01:15:09 +0300

    wip

---
+++ Homework2/FileManager/FileManager/FileManager/Crypter.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+namespace FileManager
+{
+    public static class Crypter
+    {
+        private static string? xmlString;
+        public static string Encrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            xmlString = rsa.ToXmlString(true);
+            var encryptContent = rsa.Encrypt(Encoding.UTF8.GetBytes(data), true);
+            return Convert.ToBase64String(encryptContent);
+        }
+        
+        public static string Decrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(xmlString);
+            var decryptContent = rsa.Decrypt(Convert.FromBase64String(data), true);
+            return Encoding.UTF8.GetString(decryptContent);
+        }
+    }
+}
---
+++ Homework2/PerfectNumbers/PerfectNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿var perfectNumbers = Enumerable.Range(2, 25)
+    .Where(x => Enumerable.Range(2, (int)Math.Sqrt(Math.Pow(2, x)) + 1)
+        .Where(divisor => ((int)Math.Pow(2, x) - 1) % divisor == 0)
+        .Sum() == 0)
+    .AsParallel()
+    .Select(x => Math.Pow(2, x - 1) * (Math.Pow(2, x) - 1))
+    .ToArray();
+
+
+foreach (var perfectNumber in perfectNumbers)
+{
+    Console.WriteLine(perfectNumber);
+}
