d60fbc22eed5f70c34cd9f625b03a8aa88f37353
rnretina <92212590+oveeernight@users.noreply.github.com>
2022-10-23 19:18:17 +0300

    implemented required dictionaries

---
+++ Homework4/PFMBusinessLogic/PFMBusinessLogic/FilesParser.cs
@@ -0,0 +1,211 @@
+﻿using System.Globalization;
+
+namespace PFMBusinnecLogic;
+
+public class FilesParser
+{
+    public static Dictionary<string, string> GetTitleByMovieCode()
+    {
+        var processedCodes = new HashSet<string>();
+        var lines = File.ReadAllLines("MovieCodes_IMDB.tsv");
+        return lines.Skip(1).AsParallel()
+            .Where(line =>
+            {
+                if (line.Contains("US") || line.Contains("RU"))
+                {
+                    var code = line[..line.IndexOf('\t')];
+                    lock (processedCodes)
+                    {
+                        if (processedCodes.Contains(code)) return false;
+                        processedCodes.Add(code);
+                        return true;
+                    }
+                }
+
+                return false;
+            })
+            .ToDictionary(keySelector: line => line[..line.IndexOf('\t')], elementSelector: line =>
+            {
+                var span = line.AsSpan();
+                var i = span.IndexOf('\t');
+                span = span[(i + 1)..];
+                i = span.IndexOf('\t');
+                span = span[(i + 1)..];
+                i = span.IndexOf('\t');
+                var title = span[..i].ToString();
+                //var title = span.Slice(i + 1).Slice(i + 1).Slice(0,i).ToString();
+                return title;
+            });
+    }
+
+    // key - movie id, value - list of roles
+    public static (Dictionary<string, List<string>>, Dictionary<string, string>) GetActorsAndDirectorsByMovieId()
+    {
+        var lines = File.ReadAllLines("ActorsDirectorsCodes_IMDB.tsv");
+        var actors = new Dictionary<string, List<string>>();
+        var directors = new Dictionary<string, string>();
+        Parallel.ForEach(lines, line =>
+        {
+            var span = line.AsSpan();
+            var i = span.IndexOf('\t');
+            var movieId = span[..i];
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            var actorId = span[..i];
+
+            i = span.IndexOf(('\t'));
+            span = span[(i + 1)..];
+
+            i = span.IndexOf(('\t'));
+            var role = span[..i].ToString();
+            if (role is "actor" or "actress")
+            {
+                lock (actors)
+                {
+                    if (actors.ContainsKey(movieId.ToString()))
+                    {
+                        actors[movieId.ToString()].Add(actorId.ToString());
+                    }
+                    else
+                    {
+                        actors.Add(movieId.ToString(), new List<string> { actorId.ToString() });
+                    }
+                }
+            }
+            else if (role is "director")
+            {
+                lock (directors)
+                {
+                    if (!directors.ContainsKey(movieId.ToString()))
+                    {
+                        directors.Add(movieId.ToString(),  actorId.ToString());
+                    }
+                }
+                
+            }
+        });
+        
+        return (actors, directors);
+    }
+
+    // key - actorId, value - list of filmIds, name
+    public static Dictionary<string, (string, List<string>)> GetStarredFilmsAndTitleByActorId()
+    {
+        var lines = File.ReadAllLines("ActorsDirectorsNames_IMDB.txt");
+        var result = new Dictionary<string, (string, List<string>)>();
+        Parallel.ForEach(lines, line =>
+        {
+            var span = line.AsSpan();
+            var i = span.IndexOf('\t');
+            var actorId = span[..i];
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            var name = span[..i];
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            span = span[(i + 1)..];
+
+            i = span.IndexOf('\t');
+            span = span[(i + 1)..];
+
+            var movies = new List<string>();
+            while (span.IndexOf(',') > 0)
+            {
+                var index = span.IndexOf(',');
+                movies.Add(span[..index].ToString());
+                span = span[(index + 1)..];
+            }
+
+            lock (result)
+            {
+                result.Add(actorId.ToString(), (name.ToString(), movies));
+            }
+        });
+        return result;
+    }
+
+    public static Dictionary<string, string> GetRatingByMovieId()
+    {
+        var lines = File.ReadAllLines("Ratings_IMDB.tsv");
+        return lines.Skip(1).AsParallel().ToDictionary(keySelector: line => line[..line.IndexOf('\t')], elementSelector:
+            line =>
+            {
+                var i = line.IndexOf('.');
+                return line[(i - 1)..(i + 2)];
+            });
+    }
+
+    // key - movieId, value - list of relevant tags
+    public static Dictionary<string, List<string>> GetRelevantTagsByMovieLensId()
+    {
+        var result = new Dictionary<string, List<string>>();
+        var lines = File.ReadAllLines("TagScores_MovieLens.csv").Skip(1);
+        Parallel.ForEach(lines, line =>
+        {
+            var span = line.AsSpan();
+            var i = span.IndexOf(',');
+            var movieId = span[..i].ToString();
+            span = span[(i + 1)..];
+
+            i = span.IndexOf(',');
+            var tagId = span[..i];
+            span = span[(i + 1)..];
+
+            var relevance = double.Parse(span.ToString(), CultureInfo.InvariantCulture);
+            if (relevance > 0.5)
+            {
+                lock (result)
+                {
+                    if (result.ContainsKey(movieId))
+                    {
+                        result[movieId].Add(tagId.ToString());
+                    }
+                    else
+                    {
+                        result.Add(movieId, new List<string> { tagId.ToString() });
+                    }
+                }
+            }
+        });
+        return result;
+    }
+
+    public static Dictionary<string, string> GetMovieLensIdByImdbId()
+    {
+        var lines = File.ReadAllLines("links_IMDB_MovieLens.csv");
+        return lines.Skip(1).AsParallel().ToDictionary(keySelector: line =>
+        {
+            var span = line.AsSpan();
+            var i = span.IndexOf(',');
+            span = span[(i + 1)..];
+            i = span.IndexOf(',');
+            return span[..i].ToString();
+        }, elementSelector: line => line[..line.IndexOf(',')]);
+        
+        
+        // elementSelector: line =>
+        // {
+        //     var span = line.AsSpan();
+        //     var i = span.IndexOf(',');
+        //     span = span[(i + 1)..];
+        //     i = span.IndexOf(',');
+        //     return span[..i].ToString();
+        // });
+    }
+
+    public static Dictionary<string, string> GetTagById()
+    {
+        var lines = File.ReadAllLines("TagCodes_MovieLens.csv");
+        return lines.Skip(1).AsParallel().ToDictionary(keySelector: line => line[..line.IndexOf(',')],
+            elementSelector: line => line[(line.IndexOf(',') + 1)..]);
+    }
+}
---
+++ Homework4/PFMBusinessLogic/PFMBusinessLogic/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Globalization;
+using PFMBusinnecLogic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var timer = new Stopwatch();
+        timer.Start();
+        var (actors, directors) = FilesParser.GetActorsAndDirectorsByMovieId();
+        var codesToTitles = FilesParser.GetTitleByMovieCode();
+        var tagCodesToTitles = FilesParser.GetTagById();
+        var codesToRatings = FilesParser.GetRatingByMovieId();
+        var movieLensCodeToTagIds = FilesParser.GetRelevantTagsByMovieLensId();
+        var imdbToMovieLensIds = FilesParser.GetMovieLensIdByImdbId();
+        var actorIdsToNameAndStarredFilms = FilesParser.GetStarredFilmsAndTitleByActorId();
+
+        var movies = GetMovies(codesToTitles: codesToTitles,
+            codesToActors: actors,
+            codesToDirectors: directors,
+            codesToRatings: codesToRatings,
+            tagCodesToTitles: tagCodesToTitles,
+            imdbToMovieLensIds: imdbToMovieLensIds,
+            movieLensCodesToTagIds: movieLensCodeToTagIds);
+
+        var actorsDct = GetActorsAndDirectors(movies: movies,
+            actorIdsToNameAndStarredFilms: actorIdsToNameAndStarredFilms,
+            codesToTitles: codesToTitles
+        );
+
+        var tags = GetTags(movies);
+        timer.Stop();
+        var el = timer.Elapsed;
+        Console.WriteLine($"{el.Seconds}:{el.Milliseconds}");
+    }
+
+    public static Dictionary<string, Movie> GetMovies(Dictionary<string, string> codesToTitles,
+        Dictionary<string, string> codesToRatings,
+        Dictionary<string, List<string>> movieLensCodesToTagIds,
+        Dictionary<string, string> tagCodesToTitles,
+        Dictionary<string, List<string>> codesToActors,
+        Dictionary<string, string> codesToDirectors,
+        Dictionary<string, string> imdbToMovieLensIds)
+    {
+        var result = new Dictionary<string, Movie>();
+        foreach (var (key, value) in codesToTitles)
+        {
+            var movie = GetMovieById(key, codesToTitles: codesToTitles,
+                codesToActors: codesToActors,
+                codesToDirectors: codesToDirectors,
+                codesToRatings: codesToRatings,
+                tagCodesToTitles: tagCodesToTitles,
+                imdbToMovieLensIds: imdbToMovieLensIds,
+                movieLensCodesToTagIds: movieLensCodesToTagIds);
+            if (!result.ContainsKey(value))
+                result.Add(value, movie);
+        }
+
+        return result;
+    }
+
+    private static Dictionary<string, HashSet<Movie>> GetActorsAndDirectors(
+        Dictionary<string, Movie> movies,
+        Dictionary<string, (string, List<string>)> actorIdsToNameAndStarredFilms,
+        Dictionary<string, string> codesToTitles)
+    {
+        var result = new Dictionary<string, HashSet<Movie>>();
+        foreach (var (_, (name, starredFilmIds)) in actorIdsToNameAndStarredFilms)
+        {
+            var actorMovies = new HashSet<Movie>();
+            foreach (var movie in starredFilmIds.Select(filmId =>
+                         codesToTitles.ContainsKey(filmId) && movies.ContainsKey(codesToTitles[filmId])
+                             ? movies[codesToTitles[filmId]]
+                             : null))
+            {
+                if (movie != null) actorMovies.Add(movie);
+            }
+
+            if (!result.ContainsKey(name))
+                result.Add(name, actorMovies);
+        }
+
+        return result;
+    }
+
+    public static Dictionary<string, HashSet<Movie>> GetTags(Dictionary<string, Movie> movies)
+    {
+        var result = new Dictionary<string, HashSet<Movie>>();
+        foreach (var (title, movie) in movies)
+        {
+            foreach (var tag in movie.Tags)
+            {
+                if (result.ContainsKey(tag))
+                {
+                    result[tag].Add(movies[title]);
+                }
+                else
+                {
+                    result.Add(tag, new HashSet<Movie> { movies[title] });
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+
+
+
+
+private static Movie GetMovieById(string key, Dictionary<string, string> codesToTitles,
+        Dictionary<string, string> codesToRatings,
+        Dictionary<string, List<string>> movieLensCodesToTagIds,
+        Dictionary<string, string> tagCodesToTitles,
+        Dictionary<string, List<string>> codesToActors,
+        Dictionary<string, string> codesToDirectors,
+        Dictionary<string, string> imdbToMovieLensIds)
+    {
+        if (!codesToTitles.ContainsKey(key)) return new Movie();
+        return new Movie
+        {
+            Title = codesToTitles[key],
+            Rate = codesToRatings.ContainsKey(key) ? codesToRatings[key] : null,
+            Director = codesToDirectors.ContainsKey(key) ? codesToDirectors[key] : null,
+            Actors = codesToActors.ContainsKey(key) ? codesToActors[key].ToHashSet() : new HashSet<string>(),
+            Tags = imdbToMovieLensIds.ContainsKey(key)
+                ? movieLensCodesToTagIds[imdbToMovieLensIds[key]].Select(id => tagCodesToTitles[id]).ToHashSet()
+                : new HashSet<string>()
+        };
+    }
+}
+
